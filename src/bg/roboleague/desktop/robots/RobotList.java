ef66e6dba23674781beb045566b95f76d4b48c9a
lz1irq <ivaylo.markoff@gmail.com>
2014-12-14 14:13:49 +0200

    You can set multiple ways to export the list of robots and invoke them all with one function call.

--- src/bg/roboleague/desktop/robots/RobotList.java
+++ src/bg/roboleague/desktop/robots/RobotList.java
@@ -1,11 +1,18 @@
 package bg.roboleague.desktop.robots;
 
+import bg.roboleague.desktop.robots.data.*;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class RobotList extends ArrayList<Robot> {
 
+	private List<RobotExporter> exporters;
+	
 	public RobotList() {
 		super();
+		exporters = new ArrayList<RobotExporter>();
 	}
 	
 	public void add(String name) {
@@ -18,5 +25,15 @@ public class RobotList extends ArrayList<Robot> {
 			if(robot.getName().equals(name)) result = robot;
 		}
 		return result;
+	}
+	
+	public void addExporter(RobotExporter exporter) {
+		exporters.add(exporter);
+	}
+	
+	public void export() throws IOException {
+		for(RobotExporter exporter: exporters) {
+			exporter.export(this);
+		}
 	}
 }
