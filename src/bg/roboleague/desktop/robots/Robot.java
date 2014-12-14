537dc0d87bccbed6dcf822cc0e98487904d4abb0
lz1irq <ivaylo.markoff@gmail.com>
2014-12-14 12:25:53 +0200

    Basic robot class

---
+++ src/bg/roboleague/desktop/robots/Robot.java
@@ -0,0 +1,28 @@
+package bg.roboleague.desktop.robots;
+
+public class Robot {
+	public final static int LAPS = 3;
+	private String name;
+	private int lapTimes[];
+	
+	public Robot(String rName) {
+		name = rName;
+		lapTimes =  new int[LAPS];
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getLapTime(int lap) {
+		return lapTimes[lap];
+	}
+	
+	public void setLapTime(int lap, int time) {
+		lapTimes[lap] = time;
+	}
+}
